04ad68a9d970768901cecacb1beb04a8b861664c
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-09-26 15:20:42 +0800

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,6 @@
+//eq=calc.calculate(request.query.a1,request.query.a2,request.query.a3,request.query.a4,request.query.a5);
+ 
+ //store result in db
+ //var data=calc.getId(request.query.name,request.query.fbid,request.query.timestamp,request.query.location,request.query.a1,request.query.a2,request.query.a3,request.query.a4,request.query.a5,eq);
+ 
+ 
