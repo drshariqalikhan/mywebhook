8f668b61fc8821471930f69b498f47d2faa94048
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-09-26 15:19:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
-
+const calc = require('./calc.js');
 app.set('port', (process.env.PORT || 5000));
+var eq;
 
 
 app.get('/', function(request, response) {
  
+ eq=calc.calculate(request.query.a1,request.query.a2,request.query.a3,request.query.a4,request.query.a5);
+ 
+ //store result in db
+ var data=calc.getId(request.query.name,request.query.fbid,request.query.timestamp,request.query.location,request.query.a1,request.query.a2,request.query.a3,request.query.a4,request.query.a5,eq);
+ 
   response.json({
  "messages": [
-   {"text": request.query.name},
-   {"text": request.query.id}
+   {"text": "your QOL is "+ eq},
+   {"text": "Thanks!"}
  ]
 });
 });
