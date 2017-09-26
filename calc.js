74ad2e38ec78524ca69db3bdf5b4990671376f7a
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-09-26 15:26:58 +0800

    Update calc.js

--- calc.js
+++ calc.js
@@ -3,4 +3,11 @@
  //store result in db
  //var data=calc.getId(request.query.name,request.query.fbid,request.query.timestamp,request.query.location,request.query.a1,request.query.a2,request.query.a3,request.query.a4,request.query.a5,eq);
  
- 
+ var getvalue = function(qno,answer){};
+
+ module.exports =
+  {
+      calculate: function(){};
+      getId: function(){};
+    
+  };
